75b0721b8dc6a27f3aed3683e96109e0000a80aa
danamurphy48 <danamurphy48@gmail.com>
2020-04-24 09:12:36 -0500

    For Loop every increment

--- CriticalThinkingWorksheet/CriticalThinkingWorksheet/Program.cs
+++ CriticalThinkingWorksheet/CriticalThinkingWorksheet/Program.cs
@@ -10,6 +10,11 @@ namespace CriticalThinkingWorksheet
     {
         static void Main(string[] args)
         {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
         }
     }
 }
