6317562d9ac0a42927a0b26f8c8d0275f229d9d2
danamurphy48 <danamurphy48@gmail.com>
2020-04-26 14:54:22 -0500

    Created Calculator Method class

---
+++ CriticalThinkingWorksheet/CriticalThinkingWorksheet/CalculatorMadness.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CriticalThinkingWorksheet
+{
+    class CalculatorMadness
+    {
+        public void AddTwoNumbers()
+        {
+            Console.WriteLine("Please enter the first number:");
+            int numberOne = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter the second number:");
+            int numberTwo = int.Parse(Console.ReadLine());
+
+            int result = numberOne + numberTwo;
+            Console.WriteLine("The result of this addition is: " + result);
+        }
+    }
+}
